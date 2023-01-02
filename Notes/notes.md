08c2e21c2d8ac23422f7960afd96587a5fb27c02
WesleyMatthee <wrmatthee@gmail.com>
2023-01-02 19:34:16 +0000

    Add notes.md file for generic team notes

---
+++ Notes/notes.md
@@ -0,0 +1,22 @@
+## Routes
+
+## Stories
+
+## Get a list of Stories
+`get '/stories/`
+
+## Create a stories
+`get /stories/new`
+`post /stories`
+
+## See a stories
+`get /stories/:id`
+
+
+## Update a stories
+`get /stories/:id`
+`post /stories/:id`
+
+## Delete a stories
+`delete /stories/:id`
+`post /stories/:id/delete`
--- server.js
+++ server.js
@@ -15,7 +15,12 @@ app.set('view engine', 'ejs');
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
 app.use(morgan('dev'));
+//body parser
 app.use(express.urlencoded({ extended: true }));
+
+//json parser FOR SINGLE PAGE MODEL
+app.use(express.json());
+
 app.use(
   '/styles',
   sassMiddleware({
