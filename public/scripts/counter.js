e5286a0c74aa1da8da8ccd1c961ba690c9060547
WesleyMatthee <wrmatthee@gmail.com>
2023-01-05 18:12:34 +0000

    Changed id='contribution' back to class='contribution '

--- public/scripts/counter.js
+++ public/scripts/counter.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
   console.log('Tommy can you see me?');
 
   $("#story-content").on("input", function() {
-    console.log($(this));
 
     const charCount = $(this).val().length;
     const remainingChar = 200 - charCount;
