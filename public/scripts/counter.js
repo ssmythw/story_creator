383883b9d8429845bbf94173b17eb8fd2c2a5438
WesleyMatthee <wrmatthee@gmail.com>
2023-01-05 21:39:30 +0000

    Add error event handlers for counter.js

--- public/scripts/counter.js
+++ public/scripts/counter.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
-  console.log('Tommy can you see me?');
+  // console.log('Tommy can you see me?');
 
+  //CHARACTER COUNTER
   $("#story-content").on("input", function() {
 
     const charCount = $(this).val().length;
@@ -17,11 +18,62 @@ $(document).ready(function() {
   });
 
 
+  //RESET COUNTER ON CLICK
   $("#reset").on("click", function() {
-    console.log('Tommy two');
+    // console.log('Tommy can you hear me?');
     const counter = $(this).parent().parent().find(".counter");
     counter.text(200);
   });
 
+
+
+  //ERROR HANDLING FOR TITLE TEXTAREA
+  $('.new-story-form').submit(function(event) {
+    // event.preventDefault(); ONLY FOR AJAX
+    const form = $(this);
+
+
+    console.log('Hello from Title');
+
+    const title = $(this).find("#story-title").val();
+    console.log('title', title);
+
+    if (title.length === 0) {
+
+      event.preventDefault()
+      return $('.error-text').text('Title appears to be empty!').slideDown(600);
+
+    }
+    if (title.length > 35) {
+      event.preventDefault()
+      return $('.error-text').text('Title cannot exceed 35 characters!').slideDown(600);
+    }
+
+
+    const text = $(this).find("#story-content").val();
+
+    if (text.length === 0) {
+
+      event.preventDefault()
+      return $('.error-text').text('Story appears to be empty!').slideDown(600);
+
+    }
+    if (text.length > 35) {
+      event.preventDefault()
+       return $('.error-text').text('Story cannot exceed 35 characters!').slideDown(600);
+
+    }
+
+
+
+    //BROWSER POST SO ANYTHING GOES
+
+
+  });
+
+
+
+
+  // end
 });
 
