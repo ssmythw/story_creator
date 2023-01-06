84a23f6c023edac773e5b77ad67ec6d342780dda
WesleyMatthee <wrmatthee@gmail.com>
2023-01-06 02:47:10 +0000

    Add upvote.js

---
+++ public/scripts/upvote.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+ // console.log('Tommy can you see me?');
+
+
+  $("#dataTable tbody tr").on("click", function() {
+
+
+    console.log($(this).text());
+  });
+
+});
